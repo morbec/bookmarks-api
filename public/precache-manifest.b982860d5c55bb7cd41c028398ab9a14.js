51ad01e3b9b5ae7918600ee1af6cd26cae1d0ca5
Fabio Morbec <morbecfm@gmail.com>
2019-06-08 12:02:05 +0200

    New build

---
+++ public/precache-manifest.b982860d5c55bb7cd41c028398ab9a14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442a26ac1c85cb546ee8c53dc7f83221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97669f7c7cd9bf2c641",
+    "url": "/static/css/main.3e432509.chunk.css"
+  },
+  {
+    "revision": "79f0b6177ba857863c75",
+    "url": "/static/js/2.947a5e79.chunk.js"
+  },
+  {
+    "revision": "e97669f7c7cd9bf2c641",
+    "url": "/static/js/main.99e65276.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
