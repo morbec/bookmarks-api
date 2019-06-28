4c4164dc2068f015ee7575f3dd0a9e17eb931495
Fabio Morbec <morbecfm@gmail.com>
2019-06-28 17:43:09 +0200

    Remove React stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,28 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es6: true,
-    node: true
+    browser: false
   },
-  extends: [ 'airbnb', 'eslint:recommended', 'plugin:react/recommended', 'plugin:jsx-a11y/recommended' ],
+  extends: ['airbnb', 'eslint:recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: false
     },
     ecmaVersion: 2018,
     sourceType: 'module'
   },
   parser: 'babel-eslint',
-  plugins: [ 'react', 'jsx-a11y', 'react-hooks' ],
+  plugins: [],
   rules: {
-    'react/prop-types': [ 0 ],
-    'react/no-array-index-key': [ 0 ],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/destructuring-assignment': [ 'off', { ignoreClassFields: true } ],
-    // 'react/jsx-filename-extension': [ 1, { extensions: [ '.js', '.jsx' ] } ],
-    'import/newline-after-import': [ 'off', { count: 2 } ],
+    'import/newline-after-import': ['off', { count: 2 }],
     'eslint.autoFixOnSave': true,
     'eslint.alwaysShowStatus': true,
-    'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error',
     'constructor-super': 'warn',
-    'no-this-before-super': 'warn',
-    'jsx-a11y/label-has-associated-control': [
-      2,
-      {
-        labelComponents: [ 'CustomInputLabel' ],
-        labelAttributes: [ 'label' ],
-        controlComponents: [ 'CustomInput' ],
-        depth: 3
-      }
-    ]
+    'no-this-before-super': 'warn'
   }
 }
