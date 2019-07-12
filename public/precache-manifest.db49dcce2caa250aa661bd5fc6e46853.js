988558cdb096dd348e93f5456fc6f625a581177b
Fabio Morbec <morbecfm@gmail.com>
2019-07-12 11:28:41 +0200

    New app version

---
+++ public/precache-manifest.db49dcce2caa250aa661bd5fc6e46853.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86342dc8f9c0efdc4a2d765770626855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb01d59269608f45e09",
+    "url": "/static/css/main.f6c4abd1.chunk.css"
+  },
+  {
+    "revision": "2b59215d0422fe0de67c",
+    "url": "/static/js/2.3eeb2ea1.chunk.js"
+  },
+  {
+    "revision": "0cb01d59269608f45e09",
+    "url": "/static/js/main.6202ae26.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
